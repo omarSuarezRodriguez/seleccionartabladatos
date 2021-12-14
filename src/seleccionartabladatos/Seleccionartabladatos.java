a4224d3e917e1e4a0f3f338988351766087d5a70
omarSuarezRodriguez <omar.suarez.rod@gmail.com>
2021-12-14 18:23:54 -0500

    Se sube proyecto de java que sirve para seleccionar una opción de una tabla y mostrar los datos en un settext y obtener la información

---
+++ src/seleccionartabladatos/Seleccionartabladatos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seleccionartabladatos;
+
+/**
+ *
+ * @author Omar
+ */
+public class Seleccionartabladatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Instanciamos objeto de la clase FrameSeleccionarTablaFatos
+        FrameSeleccionarTablaDatos frameSeleccionarTablaDatos = new FrameSeleccionarTablaDatos();
+        
+        // Mostramos el frame del objeto frameSeleccionarTablaDatos
+        // en pantalla
+        frameSeleccionarTablaDatos.setVisible(true);
+        
+        
+        
+    }
+    
+}
